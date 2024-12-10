de0da0f9803bed6b0ac00f20ef5b35eaca3b8ea3
syariil <syahrilditoro@gmail.com>
2024-12-10 12:11:27 +0700

    remove forgot password

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -41,9 +41,6 @@
                                 class=" border   rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                                 required="">
                         </div>
-                        <a href="#"
-                            class="text-lg font-medium text-gray-100 hover:underline text-primary-500">Forgot
-                            password?</a>
                 </div>
                 <div class="flex justify-center w-full px-2 py-2 mx-2 my-2">
                     <button type="submit"
